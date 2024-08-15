a6445244375c77f63a0a08e987cc5ba04f8d374b
Tech_Solution <onpointdev0@gmail.com>
2024-08-15 13:14:52 +0300

    Category(Controller&Service) updated

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,6 +1,7 @@
 export const TYPES = {
   User: Symbol.for('User'),
   Category: Symbol.for('Category'),
-  AuthService: Symbol.for('AuthService'),CategoryService: Symbol.for('CategoryService'),
+  AuthService: Symbol.for('AuthService'),
+  CategoryService: Symbol.for('CategoryService'),
 };
 export default TYPES;
